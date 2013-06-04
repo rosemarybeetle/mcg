2558d04c321cf0c154d4512fed2070072237f85b
Rosemary Beetle <rosemarybeetle@gmail.com>
2013-06-05 00:15:38 +0200

    latest page PHP
    Last commit was WRONG. it was a separate template sub file (full width page.php. IT SHOULD have been just page.php

--- page.php
+++ page.php
@@ -4,24 +4,23 @@
 if ( !defined('ABSPATH')) exit;
 
 /**
- * Full Content Template
+ * Pages Template
  *
-   Template Name:  Full Width Page (no sidebar)
  *
- * @file           full-width-page.php
+ * @file           page.php
  * @package        Responsive 
  * @author         Emil Uzelac 
- * @copyright      2003 - 2011 ThemeID
+ * @copyright      2003 - 2012 ThemeID
  * @license        license.txt
  * @version        Release: 1.0
- * @filesource     wp-content/themes/responsive/full-width-page.php
+ * @filesource     wp-content/themes/responsive/page.php
  * @link           http://codex.wordpress.org/Theme_Development#Pages_.28page.php.29
  * @since          available since Release 1.0
  */
 ?>
 <?php get_header(); ?>
 
-        <div id="content-full" class="grid col-940">
+        <div id="content" class="grid col-620">
         
 <?php if (have_posts()) : ?>
 
@@ -36,17 +35,7 @@ if ( !defined('ABSPATH')) exit;
                 <h1 class="post-title"><?php the_title(); ?></h1>
  
                 <?php if ( comments_open() ) : ?>               
-                <div class="post-meta">
-                <?php responsive_post_meta_data(); ?>
-                
-				    <?php if ( comments_open() ) : ?>
-                        <span class="comments-link">
-                        <span class="mdash">&mdash;</span>
-                    <?php comments_popup_link(__('No Comments &darr;', 'responsive'), __('1 Comment &darr;', 'responsive'), __('% Comments &darr;', 'responsive')); ?>
-                        </span>
-                    <?php endif; ?> 
-                </div><!-- end of .post-meta -->
-                <?php endif; ?> 
+                               <?php endif; ?> 
                 
                 <div class="post-entry">
                     <?php the_content(__('Read more &#8250;', 'responsive')); ?>
@@ -83,6 +72,7 @@ if ( !defined('ABSPATH')) exit;
 
 <?php endif; ?>  
       
-        </div><!-- end of #content-full -->
+        </div><!-- end of #content -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
